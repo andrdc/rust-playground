25283cfb6de9cf62c08d2f3ce8afcc23e085cb01
Andres Ruiz <andrsruiz@gmail.com>
2020-11-25 12:30:30 -0600

    Shadowing Variables

--- src/programming-concepts/src/main.rs
+++ src/programming-concepts/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
+	println!("--- Mutable variables ---");
 	let mut x = 5;
 	println!("The value of x is: {}", x);
 	x = 6;
 	println!("The value of x is: {}", x);
+
+	println!("--- Shadowing ---");
+	let y = 5;
+	let y = y + 1;
+	let y = y * 2;
+	println!("The value of y is: {}", y);
+	let spaces = "   ";
+	let spaces = spaces.len();
+	println!("The value of spaces is: {}", spaces);
 }
