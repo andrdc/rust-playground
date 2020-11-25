418a8254b8cdc33d05f06687dbc21843351356ca
Andres Ruiz <andrsruiz@gmail.com>
2020-11-24 19:23:59 -0600

    Handle Invalid Input

--- src/guessing-game/src/main.rs
+++ src/guessing-game/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
 	println!("Guess the number!");
 
 	let secret_number = rand::thread_rng().gen_range(1, 101);
-	// println!("The secreet number is: {}", secret_number);
 
 	loop {
 		println!("Please input your guess.");
@@ -15,10 +14,15 @@ fn main() {
 		io::stdin().read_line(&mut guess).expect("Failed to read line");
 
 		// Shadowing
-		let guess: u32 = guess.trim().parse().expect("Please type a number!");
+		let guess: u32 = match guess.trim().parse() {
+			// Handling invalid inputs
+			Ok(num) => num,
+			Err(_) => continue
+		};
 
 		println!("You guessed: {}", guess);
 
+		// Compare values with Ordering
 		match guess.cmp(&secret_number) {
 			Ordering::Less => println!("Too small!"),
 			Ordering::Greater => println!("Too big!"),
